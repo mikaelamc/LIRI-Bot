6af3ff963bfe3ae126a0218e37866f7b7efaf59a
Mikaela Mckee <mikaelamckee@iMac.local>
2018-08-19 12:20:10 -0500

    Spotify,OMDB

--- liri.js
+++ liri.js
@@ -3,11 +3,29 @@ require("dotenv").config();
 // To use what we have installed, we have to require the file and create a variable
 var keys = require("./keys");
 var Spotify = require("node-spotify-api");
+var fs = require("fs");
+var request = require("request");
 // console.log(keys.spotify);
 
+// Then run a request to the OMDB API with the movie specified
+request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
+    if (!error && response.statusCode === 200){
+        console.log("The movie's rating is: " + JSON.parse(body).imdbRating);
+    }
+});
+
+// Read File for Spotify
+fs.readFile("random.txt", "utf8", function(err, data){
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log(data);
+});
 
 var spotify = new Spotify(keys.spotify);
 // var client = new Twitter(keys.twitter);
+
 // Constructors - Are functions ; Are dark green with a Capital letter when used with "New" ; Running constructors you will use "New; "
 // New Key Word - Instantiates aka creates a copy of the spotify object; A constructor function creates a new object;
 // Constructors are blue prints that creates copies of itself
@@ -18,7 +36,7 @@ var nodeArgs = process.argv;
 // process.argv is an array
 console.log(process.argv.length);
 // Obtaining user input into our code
-console.log(nodeArgs[3]);
+// console.log(nodeArgs);
 
 
 
